f1ea85fca16fee4108ecdc973e7f3b367d05eac0
Centoventi_ <78879655+centoventi@users.noreply.github.com>
2026-02-20 23:55:28 +0100

    Aggiunto il codice per la logica booleana

---
+++ BooleanTasto.c
@@ -0,0 +1,18 @@
+bool TASTO;
+void setup() {
+  pinMode(13, OUTPUT); // Configura il pin 13 come output
+  pinMode(3, INPUT);   // Configura il pin 3 come input
+}
+void loop() {
+  TASTO = digitalRead(3);
+  if (TASTO == HIGH) 
+  {
+    digitalWrite(13, HIGH); // Accendo il LED
+    delay(8000);             // Delay di 800 ms
+  } else
+  
+  
+    
+    digitalWrite(13, LOW);  // Spengo il LED
+  
+}
